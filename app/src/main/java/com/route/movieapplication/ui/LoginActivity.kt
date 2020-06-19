ecf3e2d4415ed4d9a84fb974be35661b1541971e
ahmedsabermaxab <64734712+ahmedsabermaxab@users.noreply.github.com>
2020-06-19 17:40:50 +0200

    implement login feature

---
+++ app/src/main/java/com/route/movieapplication/ui/LoginActivity.kt
@@ -0,0 +1,13 @@
+package com.route.movieapplication.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.route.movieapplication.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
